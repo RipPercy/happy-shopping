526d70b563101a78b215b4008760fbf8a2ba13c9
RipPercy <Hajooka2015@gmail.com>
2023-05-11 11:33:56 +0700

    update Prices

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
 
 //Configure env
 dotenv.config();
@@ -21,17 +20,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
 
 //Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-
-//Rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 //PORT
 const PORT = process.env.PORT || 8080;
